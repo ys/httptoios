229c08f114f3b4b716ffa05b01c47cd35339caf0
Yannick Schutz <yannick.schutz@gmail.com>
2013-03-19 12:23:16 +0100

    handle path

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 get "/" do
   "http://httptoios.yannick.io/{{scheme}} -> {{scheme}}://"
 end
+
 get "/:custom_scheme" do
   redirect "#{params[:custom_scheme]}://", 301
+end
+
+get "/:custom_scheme/:path" do
+  redirect "#{params[:custom_scheme]}://#{params[:path]}", 301
 end
